af466ceb3b5947b604d47af4465458d87a763c2f
Demes Toss <domestoss@proton.me>
2023-08-13 17:23:26 +0200

    Added scheduling for trigger simple mode on lights

--- color_change.py
+++ color_change.py
@@ -1,10 +1,14 @@
 import board
 import time
 import neopixel
+import schedule
 import subprocess as sp
 from temp_config import Temperature
 from types import SimpleNamespace
 
+TICK_TIME=8
+
+# State based on Temperature
 stateDict = { "Normal": 0, "Medium": 1, "High": 2, "Nightmare": 3 }
 State = SimpleNamespace(**stateDict)
 
@@ -19,53 +23,107 @@ def calc_state(temp):
     elif v >= Temperature.High:
         return State.Nightmare
 
-np = neopixel.NeoPixel(board.D18, 4)
+# Mode settings
+modeDict = { "Default": 0, "Simple": 1 }
+Mode = SimpleNamespace(**modeDict)
+current_mode = { "value": Mode.Default }
 
+def set_simple_mode():
+    current_mode["value"] = Mode.Simple
+
+def set_default_mode():
+    current_mode["value"] = Mode.Default
+
+# Scheduler to change mode
+
+schedule.every().day.at("00:00").do(set_simple_mode)
+schedule.every().day.at("08:00").do(set_default_mode)
+
+# Init NeoPixel
 # https://rapidtables.com/web/color/RGB_Color.html
 
-def normal_mode():
+np = neopixel.NeoPixel(board.D18, 4)
+
+def normal_color():
     np[0] = (0, 0, 204)
     np[1] = (0, 0, 255)
     np[2] = (102, 0, 204)
     np[3] = (127, 0, 255)
 
-def warning_mode():
+def normal_color_simple():
+    np[0] = (0, 0, 204)
+    np[1] = (0, 0, 0)
+    np[2] = (0, 0, 0)
+    np[3] = (0, 0, 0)
+
+def warning_color():
     np[0] = (255, 128, 0)
     np[1] = (255, 153, 51)
     np[2] = (255, 255, 0)
     np[3] = (255, 255, 51)
 
-def hardcore_mode():
+def warning_color_simple():
+    np[0] = (0, 0, 0)
+    np[1] = (0, 0, 0)
+    np[2] = (255, 255, 0)
+    np[3] = (0, 0, 0)
+
+def hardcore_color():
     np[0] = (255, 0, 0)
     np[1] = (255, 51, 51)
     np[2] = (255, 0, 127)
     np[3] = (255, 51, 153)
 
-def nightmare_mode():
+def hardcore_color_simple():
+    np[0] = (255, 0, 0)
+    np[1] = (0, 0, 0)
+    np[2] = (0, 0, 0)
+    np[3] = (0, 0, 0)
+
+def nightmare_color():
     np[0] = (255, 0, 0)
     np[1] = (255, 0, 0)
     np[2] = (204, 0, 0)
     np[3] = (204, 0, 0)
 
+def change_color_simple(state):
+    if state == State.Normal:
+        normal_color_simple() 
+    elif state == State.Medium:
+        warning_color_simple() 
+    elif state == State.High:
+        hardcore_color_simple() 
+    elif state == State.Nightmare:
+        nightmare_color()
+
 def change_color(state):
     if state == State.Normal:
-        normal_mode() 
+        normal_color() 
     elif state == State.Medium:
-        warning_mode() 
+        warning_color() 
     elif state == State.High:
-        hardcore_mode() 
+        hardcore_color() 
     elif state == State.Nightmare:
-        nightmare_mode()
+        nightmare_color()
+
+def change_color_by_mode(state, mode):
+    if mode == Mode.Default:
+        change_color(state)
+    else:
+        change_color_simple(state)
 
 try:
     prev_state = -1
+    prev_mode = current_mode["value"]
     while True:
+        schedule.run_pending()
         temp = sp.getoutput("vcgencmd measure_temp | egrep -o '[0-9]*\.[0-9]*'")
         state = calc_state(temp)
-        if (state != prev_state):
-            change_color(state)
+        if (state != prev_state or prev_mode != current_mode["value"]):
+            change_color_by_mode(state, current_mode["value"])
         prev_state = state
-        time.sleep(8)
+        prev_mode = current_mode["value"]
+        time.sleep(TICK_TIME)
 
 except KeyboardInterrupt:
     pass
--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-python3 luma.examples/examples/sys_info.py &
-python3 rpi_tower_setup/fan_speed.py &
-sudo python3 rpi_tower_setup/color_change.py &
+python3 ~/luma.examples/examples/sys_info.py &
+python3 fan_speed.py &
+pipenv run python3 color_change.py &
 
